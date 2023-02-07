1c33908a8ab4026dca9f4b2460b48bf92cbaa993
Finley Decker <finleydecker@gmail.com>
2023-02-06 22:58:09 -0800

    example code finished

---
+++ example.js
@@ -0,0 +1,38 @@
+// Import the SDK
+const LordOfTheRingsSDK = require('./index.js');
+// Store your api key
+const apiKey = 'xbrK_NYKref0KmoTHm2k';
+
+// Create an instance of the SDK
+const sdk = new LordOfTheRingsSDK(apiKey);
+const movie = sdk.getMovieInstance();
+
+// Call the getMovies method
+movie.getMovies()
+  .then(movies => {
+    console.log(movies);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// Store ID of the movie
+const movieID = '5cd95395de30eff6ebccde5c';
+
+// Call the getMovieById method
+movie.getMovieById(movieID)
+  .then(movie => {
+    console.log(movie);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// Call the getMovieQuotesById method
+movie.getMovieQuotesById(movieID)
+  .then(quotes => {
+    console.log(quotes);
+  })
+  .catch(error => {
+    console.error(error);
+  });
