38244ccb55196b41f942bd178e0c5ca6001116b9
Finley Decker <finleydecker@gmail.com>
2023-02-07 02:00:05 -0800

    small update to apikey var

--- demo.js
+++ demo.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-console */
+
 // Import the SDK
-const LordOfTheRingsSDK = require('./index.js');
+const LordOfTheRingsSDK = require('./index');
 // Insert your api key here
-const apiKey = '<YOUR_API_KEY_HERE>';
+const apiKey = 'xbrK_NYKref0KmoTHm2k';
 
 // Create an instance of the SDK
 const sdk = new LordOfTheRingsSDK(apiKey);
@@ -9,10 +11,10 @@ const movie = sdk.getMovieInstance();
 
 // Call the getMovies method
 movie.getMovies()
-  .then(movies => {
+  .then((movies) => {
     console.log(movies);
   })
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
   });
 
@@ -21,18 +23,18 @@ const movieID = '5cd95395de30eff6ebccde5c';
 
 // Call the getMovieById method
 movie.getMovieById(movieID)
-  .then(movie => {
-    console.log(movie);
+  .then((myMovie) => {
+    console.log(myMovie);
   })
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
   });
 
 // Call the getMovieQuotesById method
 movie.getMovieQuotesById(movieID)
-  .then(quotes => {
+  .then((quotes) => {
     console.log(quotes);
   })
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
   });
