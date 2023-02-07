a81eee75ead3e5bcc9aff083aa1b2f7bb9389b53
Finley Decker <finleydecker@gmail.com>
2023-02-07 02:02:32 -0800

    removed unecessary try catch blocks

--- src/movie.js
+++ src/movie.js
@@ -5,36 +5,24 @@ const API_BASE_URL = 'https://the-one-api.dev/v2/';
 class Movie {
   constructor(axiosInstance) {
     this.axiosInstance = axiosInstance;
-  };
+  }
 
   // Method to retrieve all movies
   async getMovies() {
-    try {
-      const response = await this.axiosInstance.get('movie');
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+    const response = await this.axiosInstance.get('movie');
+    return response.data;
   }
 
   // Method to retrieve a movie by ID
   async getMovieById(id) {
-    try {
-      const response = await this.axiosInstance.get(`${API_BASE_URL}movie/${id}`);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+    const response = await this.axiosInstance.get(`${API_BASE_URL}movie/${id}`);
+    return response.data;
   }
 
   // Method to retrieve quotes from a movie by ID
   async getMovieQuotesById(id) {
-    try {
-      const response = await this.axiosInstance.get(`${API_BASE_URL}movie/${id}/quote`);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+    const response = await this.axiosInstance.get(`${API_BASE_URL}movie/${id}/quote`);
+    return response.data;
   }
 }
 
